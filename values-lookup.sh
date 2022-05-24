dbbec27e2ef40c78f5b6a6730f507b70d11fa2c5
Kevin Hansen <hansen_kevin@bah.com>
2022-05-24 09:32:34 -0500

    istio.off

---
+++ values-lookup.sh
@@ -0,0 +1,24 @@
+
+echo
+echo "from dev/config.yaml"
+(
+echo "  .Values.addons.vault.ingress.cert     = $( cat dev/configmap.yaml | yq '.addons.vault.ingress.cert' )"
+echo "  .Values.addons.vault.ingress.key      = $( cat dev/configmap.yaml | yq '.addons.vault.ingress.key' )"
+echo "  .Values.addons.vault.ingress.gateway  = $(cat dev/configmap.yaml | yq '.addons.vault.ingress.gateway' )"
+) |  tee config.yaml.results
+
+helm get values bigbang -n bigbang | tee bigbang.yaml
+echo
+echo "from bigbang-values.yaml"
+(
+echo "  .Values.addons.vault.ingress.cert     = $( cat bigbang.yaml | yq '.addons.vault.ingress.cert' )"
+echo "  .Values.addons.vault.ingress.key      = $( cat bigbang.yaml | yq '.addons.vault.ingress.key' )"
+echo "  .Values.addons.vault.ingress.gateway  = $( cat bigbang.yaml | yq '.addons.vault.ingress.gateway' )"
+) | tee bigbang-values.results
+
+sdiff ./config.yaml.results ./bigbang-values.results
+
+
+
+
+echo
