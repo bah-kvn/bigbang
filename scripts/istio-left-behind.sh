f75c3c97b8dcaf5a826fba7a903a31b5798c2e9e
Kevin Hansen <hansen_kevin@bah.com>
2022-05-25 12:33:17 -0500

    istio on

---
+++ scripts/istio-left-behind.sh
@@ -0,0 +1,3 @@
+kubectl get mutatingwebhookconfigurations,validatingwebhookconfigurations,wasmplugins,istiooperators,destinationrules,envoyfilters -A
+kubectl get gateways,proxyconfigs,serviceentries,sidecars,virtualservices,workloadentries,workloadgroups,authorizationpolicies -A
+kubectl get peerauthentications,requestauthentications,telemetries -A
