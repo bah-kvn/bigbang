9f26bb8293c0629acd8d799d02604d69b87d6c0b
Kevin Hansen <hansen_kevin@bah.com>
2022-05-25 18:04:54 -0500

    sanitized secrets

--- scripts/values-lookup.sh
+++ scripts/values-lookup.sh
@@ -7,13 +7,13 @@ echo "  .Values.addons.vault.ingress.key      = $( cat dev/configmap.yaml | yq '
 echo "  .Values.addons.vault.ingress.gateway  = $(cat dev/configmap.yaml | yq '.addons.vault.ingress.gateway' )"
 ) |  tee config.yaml.results
 
-helm get values bigbang -n bigbang | tee bigbang.yaml
+helm get values bigbang -n bigbang | tee bigbang.values
 echo
 echo "from bigbang-values.yaml"
 (
-echo "  .Values.addons.vault.ingress.cert     = $( cat bigbang.yaml | yq '.addons.vault.ingress.cert' )"
-echo "  .Values.addons.vault.ingress.key      = $( cat bigbang.yaml | yq '.addons.vault.ingress.key' )"
-echo "  .Values.addons.vault.ingress.gateway  = $( cat bigbang.yaml | yq '.addons.vault.ingress.gateway' )"
+echo "  .Values.addons.vault.ingress.cert     = $( cat bigbang.values | yq '.addons.vault.ingress.cert' )"
+echo "  .Values.addons.vault.ingress.key      = $( cat bigbang.values | yq '.addons.vault.ingress.key' )"
+echo "  .Values.addons.vault.ingress.gateway  = $( cat bigbang.values | yq '.addons.vault.ingress.gateway' )"
 ) | tee bigbang-values.results
 
 sdiff ./config.yaml.results ./bigbang-values.results
