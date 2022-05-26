9c9aeac7027f8103ae4a1ed8b393fc4c7392abce
Kevin Hansen <hansen_kevin@bah.com>
2022-05-26 15:46:57 -0500

    vault.off

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,11 @@
 
 
-#kubectl create secret tls vault-server-tls \
-#  --namespace='vault' \
-#  --cert=$HOME/certs/vault.dev.bahsoftwarefactory.com/fullchain.pem \
-#  --key=$HOME/certs/vault.dev.bahsoftwarefactory.com/privkey.pem
+kubectl create secret tls vault-server-tls \
+  --namespace='vault' \
+  --cert=$HOME/certs/vault.dev.bahsoftwarefactory.com/fullchain.pem \
+  --key=$HOME/certs/vault.dev.bahsoftwarefactory.com/privkey.pem
 
-#( kubectl exec -i -t vault-vault-0 -- vault operator init ) | tee init
+( kubectl exec -i -t vault-vault-0 -- vault operator init ) | tee init
 
 
 cat init | grep ':' | sed 's/Initial Root Token: /export VAULT_ROOT_TOKEN=/g' | grep export | tee set_root_token.sh
