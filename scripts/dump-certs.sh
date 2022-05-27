b6aad298a3598491c728384f360fe19d7cb0e6b2
Kevin Hansen <hansen_kevin@bah.com>
2022-05-27 15:07:11 -0500

    fixed cert for istio.gateway

---
+++ scripts/dump-certs.sh
@@ -0,0 +1,18 @@
+
+
+
+sops -d base/secrets.enc.yaml | yq '.stringData."values.yaml"' | yq '.istio.vault.tls.cert' > /tmp/istio_vault_tls_cert.cert
+sops -d base/secrets.enc.yaml | yq '.stringData."values.yaml"' | yq '.istio.gateways.public.tls.cert' > /tmp/istio_gateways_tls_cert.cert
+sops -d base/secrets.enc.yaml | yq '.stringData."values.yaml"' | yq '.addons.vault.ingress.cert' > /tmp/addons_vault_ingress_cert.cert 
+echo istio.vault.tls.cert
+openssl x509 -in /tmp/istio_vault_tls_cert.cert -text | grep 'Subject: CN='
+echo
+echo istio.gateways.tls.cert
+openssl x509 -in /tmp/istio_gateways_tls_cert.cert -text | grep 'Subject: CN='
+echo
+echo addons.vault.ingress.cert
+openssl x509 -in /tmp/addons_vault_ingress_cert.cert -text | grep 'Subject: CN='
+echo
+
+
+
