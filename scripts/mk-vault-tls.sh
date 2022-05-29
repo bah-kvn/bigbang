044ef4333df01740c0a4cf2166c6e49cd5aecd58
Kevin Hansen <hansen_kevin@bah.com>
2022-05-29 17:16:37 -0500

    ui via passthrough - finally

--- scripts/mk-vault-tls.sh
+++
@@ -1,6 +0,0 @@
-
-
-kubectl create secret tls vault-server-tls \
-  --namespace='vault' \
-  --cert=$HOME/certs/vault.stg.bahsoftwarefactory.com/fullchain.pem \
-  --key=$HOME/certs/vault.stg.bahsoftwarefactory.com/privkey.pem
